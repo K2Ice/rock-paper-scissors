f06df585575c2a99974d61fe6eacc970d8a5fadc
Konrad <k.korzeniewski94@gmail.com>
2022-10-19 03:00:45 +0200

    Write the game logic

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ function getComputerChoice(){
 
     switch(randomShape){
         case 0:
-        return "Rock";
+        return "rock";
         case 1:
-        return "Paper";
+        return "paper";
         case 2:
-        return "Scissors";
+        return "scissors";
     }
 }
+
+const playerSelection = prompt('Write you shape:')
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection)
+    console.log(computerSelection)
+    switch(playerSelection){
+        case 'rock':
+            if(computerSelection === 'rock') return 'draw'
+            else if (computerSelection === 'paper') return 'lose'
+            else return 'win'
+        case 'paper':
+            if(computerSelection === 'rock') return 'win'
+            else if (computerSelection === 'paper') return 'draw'
+            else return 'lose'
+        case 'scissors':
+            if(computerSelection === 'rock') return 'lose'
+            else if (computerSelection === 'paper') return 'win'
+            else return 'draw'
+    }
+}
+console.log(playRound(playerSelection, computerSelection))
